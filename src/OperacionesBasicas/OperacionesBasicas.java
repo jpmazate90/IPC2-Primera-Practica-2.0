7e0b6f59d72046031be093c031d6c696c933ec80
jpmazate90 <juanpablo-orizabalgil@cunoc.edu.gt>
2018-07-25 21:51:46 -0600

    Se implementaron las operaciones basicas suma, resta, raiz, y potencia, ademas de un metodo para mostrar si hubo algun error. No se implemento el llamado a los metodos

---
+++ src/OperacionesBasicas/OperacionesBasicas.java
@@ -0,0 +1,61 @@
+
+package OperacionesBasicas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jpmazate
+ */
+public class OperacionesBasicas {
+    
+    public double operacionSuma(double valores[],int numeroSumas){
+        double resultado=0;
+        try{
+            for(int i=0;i<numeroSumas;i++){
+                resultado+=valores[i];
+            }
+        }catch(Exception e){
+            mostrarMensaje("Suma");
+        }
+        return resultado;
+    }
+    
+    public double operacionResta(double primerValor, double segundoValor){
+        double resultado;
+        try{
+            resultado= primerValor-segundoValor;
+            return resultado;
+        }catch(Exception e){
+            mostrarMensaje("Resta");
+            return 0;
+        }
+    }
+    
+    public double operacionPotencia(double valorIndice, double valorAplicado){
+        double resultado;
+        try{
+            resultado= Math.pow(valorAplicado, valorIndice);
+            return resultado;
+        }catch(Exception e){
+            mostrarMensaje("Potencia");
+            return 0;
+        }
+    }
+    
+    public double operacionRaiz(double valorExponente, double valorAplicado){
+        double resultado;
+        double comodinRaiz=1;
+        try {
+            resultado= Math.pow(valorAplicado,comodinRaiz/valorExponente);
+            return resultado;
+        } catch (Exception e) {
+            mostrarMensaje("Raiz");
+            return 0;
+        }
+    }
+    
+    public void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(null,"Hubo un error al procesar tu operacion "+mensaje+" mostraremos un 0");
+    }
+}
