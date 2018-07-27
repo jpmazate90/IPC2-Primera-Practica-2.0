e7120e95374ee2e7e6501860ba9a8c06278ec69d
Carlos Soberanis <JCSRU13@gmail.com>
2018-07-27 13:25:34 -0600

    Error en una operacion convertidora

--- src/Operaciones/OperacionesConvertidoras.java
+++ src/Operaciones/OperacionesConvertidoras.java
@@ -6,14 +6,14 @@ package Operaciones;
  * @author jpmazate
  */
 public class OperacionesConvertidoras {
-    
+   
     public int opcionDecimalABinario(int numero){
         String resultadoEnString;
         int resultado;
         try{
             resultadoEnString = Integer.toBinaryString(numero);
             resultado = Integer.parseInt(resultadoEnString);
-        return resultado;
+            return resultado;
         }catch(NumberFormatException e){
             OperacionesBasicas.mostrarMensaje("Decimal a Binario");
             return 0;
